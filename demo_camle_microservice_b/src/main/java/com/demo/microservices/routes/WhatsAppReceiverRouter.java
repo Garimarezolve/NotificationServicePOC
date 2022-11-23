930f48915ed0490fbbb1c5ff6b0cac483982c76e
garimas <GarimaSharma@resolve.com>
2022-11-23 11:11:56 +0530

    whats app integration

---
+++ demo_camle_microservice_b/src/main/java/com/demo/microservices/routes/WhatsAppReceiverRouter.java
@@ -0,0 +1,21 @@
+package com.demo.microservices.routes;
+
+import com.demo.microservices.dto.UserChannel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WhatsAppReceiverRouter extends RouteBuilder {
+
+    @Autowired
+    private UserChannelProcessor userChannelProcessor;
+
+    @Override
+    public void configure() throws Exception {
+            from("activemq:my-activemq-queue").unmarshal().json(JsonLibrary.Jackson, UserChannel.class)
+                    .bean(userChannelProcessor).to("log:received-message-from-active-mq");
+
+        }
+}
