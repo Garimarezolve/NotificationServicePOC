6b8a58b42c17f45a6697622119c8bba03e568dd4
garimas <GarimaSharma@resolve.com>
2022-11-23 16:16:35 +0530

    Mongodb  configuration  file

---
+++ demo_camle_microservice_b/src/main/java/com/demo/microservices/config/MongoConfig.java
@@ -0,0 +1,21 @@
+package com.demo.microservices.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+
+public class MongoConfig {
+    @Bean("mongoBean")
+    public MongoClient getMongoClient(){
+        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/Notification_db");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+}
+
